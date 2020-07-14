d46522c7f7ea21058a849d3ae1aac34a14c67e20
SoyDiego <diegofranchina@gmail.com>
2020-07-14 23:00:06 +0200

    Added Component AddCategory and communicate between them

--- 01-gif-expert-app/src/GifExpertApp.jsx
+++ 01-gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
+	// const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+
+	const [categories, setCategories] = useState([
+		"One Punch",
+		"Samurai X",
+		"Dragon Ball",
+	]);
+
 	return (
 		<>
 			<h2>GifExpertApp</h2>
+			<AddCategory setCategories={setCategories}/>
 			<hr />
+
+			<ol>
+				{categories.map((category) => (
+					<li key={category}>{category}</li>
+				))}
+			</ol>
 		</>
 	);
 };
