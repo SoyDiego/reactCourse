97b79dbd81445b538a1989b35e52976bf17b81f1
SoyDiego <diegofranchina@gmail.com>
2020-07-17 17:19:31 +0200

    Tested GifExpertApp.jsx

--- 01-gif-expert-app/src/GifExpertApp.jsx
+++ 01-gif-expert-app/src/GifExpertApp.jsx
@@ -1,22 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AddCategory from "./components/AddCategory";
 import GifGrid from "./components/GifGrid";
 
-const GifExpertApp = () => {
+const GifExpertApp = ({ defaultCategories = [] }) => {
+	// const [categories, setCategories] = useState([
+	// 	"Dragon Ball"
+	// ]);
 
-	const [categories, setCategories] = useState([
-		"Dragon Ball"
-	]);
+	const [categories, setCategories] = useState(defaultCategories); //Para testing usar esto, para la app comentar esto y descomentar lo de arriba.
 
 	return (
 		<>
 			<h2>GifExpertApp</h2>
-			<AddCategory setCategories={setCategories}/>
+			<AddCategory setCategories={setCategories} />
 			<hr />
 
 			<ol>
 				{categories.map((category) => (
-					<GifGrid key={category} category={category}/>
+					<GifGrid key={category} category={category} />
 				))}
 			</ol>
 		</>
