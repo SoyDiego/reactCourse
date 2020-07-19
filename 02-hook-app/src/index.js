49404d1a4648435817cccf14878f16cfdb19b8a4
SoyDiego <diegofranchina@gmail.com>
2020-07-19 11:29:08 +0200

    Form with Custom Hook (useForm) done

--- 02-hook-app/src/index.js
+++ 02-hook-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 // import CounterApp from './components/01-useState/CounterApp';
 //  import HookApp from './HookApp';
 // import CounterWithCustomHooks from './components/01-useState/CounterWithCustomHooks';
-import SimpleForm from "./components/02-useEffect/SimpleForm";
+// import SimpleForm from "./components/02-useEffect/SimpleForm";
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 
 ReactDOM.render(
-  <SimpleForm/>,
+  <FormWithCustomHook/>,
   document.getElementById('root')
 );
