82435516e50ad3d3eb140be5bc7e384e011ee42f
SoyDiego <diegofranchina@gmail.com>
2020-07-22 20:40:33 +0200

    Toggle and Delete ToDoApp - UseReducer() done

--- 02-hook-app/src/components/08-useReducer/TodoApp.jsx
+++ 02-hook-app/src/components/08-useReducer/TodoApp.jsx
@@ -27,6 +27,22 @@ const TodoApp = () => {
 		localStorage.setItem("todos", JSON.stringify(todos));
 	}, [todos]);
 
+	const handleDelete = (todoId) => {
+		const action = {
+			type: "delete",
+			payload: todoId,
+		};
+
+		dispatch(action);
+	};
+
+	const handleToggle = (todoId) => {
+		dispatch({
+			type: "toggle",
+			payload: todoId,
+		});
+	};
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
@@ -58,10 +74,14 @@ const TodoApp = () => {
 					<ul className="list-group list-group-flush">
 						{todos.map((todo, index) => (
 							<li key={todo.id} className="list-group-item">
-								<p className="text-center">
+								<p
+									className={`${todo.done && "complete"}`}
+									onClick={() => handleToggle(todo.id)}>
 									{index + 1}) {todo.desc}
 								</p>
-								<button className="btn btn-danger">
+								<button
+									className="btn btn-danger"
+									onClick={() => handleDelete(todo.id)}>
 									Borrar
 								</button>
 							</li>
