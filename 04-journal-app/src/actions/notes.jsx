f348881e889c8563798e751847e0cb03dc3db0b4
SoyDiego <diegofranchina@gmail.com>
2020-08-07 12:30:16 +0200

    Purge notes on logout and refresh notes on sidebar when is added done

--- 04-journal-app/src/actions/notes.jsx
+++ 04-journal-app/src/actions/notes.jsx
@@ -16,8 +16,17 @@ export const startNewNote = () => {
 
 		const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 		dispatch(activeNote(doc.id, newNote));
+		dispatch(addNewNote(doc.id, newNote));
 	};
 };
+
+export const addNewNote = (id, note) => ({
+	type: types.notesAddNew,
+	payload: {
+		id,
+		...note,
+	},
+});
 
 export const activeNote = (id, note) => ({
 	type: types.notesActive,
@@ -105,4 +114,8 @@ export const startDeleting = (id) => {
 export const deleteNote = (id) => ({
 	type: types.notesDelete,
 	payload: id,
+});
+
+export const noteLogout = () => ({
+	type: types.notesLogoutCleaning,
 });
