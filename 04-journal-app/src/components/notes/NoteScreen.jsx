c15a0c4c7c0c35d60e9ffbc274c5ccaa6b6b9ce8
SoyDiego <diegofranchina@gmail.com>
2020-08-07 11:52:08 +0200

    Delete note done

--- 04-journal-app/src/components/notes/NoteScreen.jsx
+++ 04-journal-app/src/components/notes/NoteScreen.jsx
@@ -3,7 +3,7 @@ import { NotesAppBar } from "./NotesAppBar";
 import { useSelector, useDispatch } from "react-redux";
 import { useForm } from "../../hooks/useForm";
 import { useEffect } from "react";
-import { activeNote } from "../../actions/notes";
+import { activeNote, startDeleting } from "../../actions/notes";
 
 export const NoteScreen = () => {
 	const dispatch = useDispatch();
@@ -11,7 +11,7 @@ export const NoteScreen = () => {
 
 	const [formValues, handleInputChange, reset] = useForm(note);
 
-	const { body, title, url } = formValues;
+	const { body, title, url, id } = formValues;
 
 	const activeId = useRef(note.id);
 
@@ -25,6 +25,10 @@ export const NoteScreen = () => {
 	useEffect(() => {
 		dispatch(activeNote(formValues.id, { ...formValues }));
 	}, [formValues, dispatch]);
+
+	const handleDelete = () => {
+		dispatch(startDeleting(id));
+	};
 
 	return (
 		<div className="notes__main-content">
@@ -55,6 +59,10 @@ export const NoteScreen = () => {
 					</div>
 				)}
 			</div>
+
+			<button className="btn btn-danger" onClick={handleDelete}>
+				Delete
+			</button>
 		</div>
 	);
 };
--- 04-journal-app/src/reducers/notesReducer.jsx
+++ 04-journal-app/src/reducers/notesReducer.jsx
@@ -1,5 +1,4 @@
 import { types } from "../types/types";
-import { startSaveNote } from "../actions/notes";
 
 const initialState = {
 	notes: [],
@@ -25,6 +24,12 @@ export const notesReducer = (state = initialState, action) => {
 				notes: state.notes.map((note) =>
 					note.id === action.payload.id ? action.payload.note : note
 				),
+			};
+		case types.notesDelete:
+			return {
+				...state,
+				active: null,
+				notes: state.notes.filter((note) => note.id !== action.payload),
 			};
 
 		default:
