d0dccad19852a49aba8974f1df5a177bf1f2a1a7
SoyDiego <diegofranchina@gmail.com>
2020-08-24 21:33:18 +0200

    Shooting events on Calendar

--- 05-calendar-app/src/components/calendar/CalendarScreen.js
+++ 05-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "moment/locale/es";
 import { Navbar } from "../ui/Navbar";
 import { messages } from "../../helpers/calendar-messages-es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { CalendarEvent } from "./CalendarEvent";
 
 moment.locale("es");
 
@@ -16,10 +17,30 @@ const events = [
 		start: moment().toDate(),
 		end: moment().add(2, "hours").toDate(),
 		bgcolor: "#fafafa",
+		notes: "Comprar el pastel",
+		user: {
+			_id: "123",
+			name: "Diego",
+		},
 	},
 ];
 
 export const CalendarScreen = () => {
+	const [lastView, setLastView] = useState(
+		localStorage.getItem("lastView") || "month"
+	);
+
+	const onDoubleClick = (e) => {
+		console.log(e);
+	};
+	const onSelectEvent = (e) => {
+		console.log(e);
+	};
+	const onViewChange = (e) => {
+		setLastView(e);
+		localStorage.setItem("lastView", e);
+	};
+
 	const eventStyleGetter = (event, start, end, isSelected) => {
 		const style = {
 			backgroundColor: "#367cf7",
@@ -44,6 +65,11 @@ export const CalendarScreen = () => {
 				style={{ height: 500 }}
 				messages={messages}
 				eventPropGetter={eventStyleGetter}
+				onDoubleClickEvent={onDoubleClick}
+				onSelectEvent={onSelectEvent}
+				onView={onViewChange}
+				view={lastView}
+				components={{ event: CalendarEvent }}
 			/>
 		</div>
 	);
