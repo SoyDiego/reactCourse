5825ad3d6a991e2edb43c0416b7ffe063d3512c9
SoyDiego <diegofranchina@gmail.com>
2020-08-28 17:27:27 +0200

    installed and used expres-validator: check and get errors

--- 05-calendar-app/backend/routes/auth.js
+++ 05-calendar-app/backend/routes/auth.js
@@ -8,12 +8,37 @@ const { Router } = require("express");
 
 const router = Router();
 
-const { crearUsuario, loginUsuario, revalidarToken } = require("../controllers/auth");
+const { check } = require("express-validator");
 
-router.post("/new", crearUsuario );
+const {
+	crearUsuario,
+	loginUsuario,
+	revalidarToken,
+} = require("../controllers/auth");
 
-router.post("/", loginUsuario);
+router.post(
+	"/new",
+	[
+		check("name", "El nombre es obligatorio").not().isEmpty(),
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "El password debe de ser de 6 caracteres").isLength({
+			min: 6,
+		}),
+	],
+	crearUsuario
+);
 
-router.get("/renew", revalidarToken );
+router.post(
+	"/",
+	[
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "El password debe de ser de 6 caracteres").isLength({
+			min: 6,
+		}),
+	],
+	loginUsuario
+);
+
+router.get("/renew", revalidarToken);
 
 module.exports = router;
