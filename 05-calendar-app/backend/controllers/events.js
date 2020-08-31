810a5811481851978620f1100c6cc29985158912
SoyDiego <diegofranchina@gmail.com>
2020-08-31 11:36:35 +0200

    Created model Evento and starting with validate fields

--- 05-calendar-app/backend/controllers/events.js
+++ 05-calendar-app/backend/controllers/events.js
@@ -7,6 +7,9 @@ const getEventos = async (req, res = response) => {
 	});
 };
 const crearEvento = async (req, res = response) => {
+	//Verificar que tenga el EVENTO
+	console.log(req.body);
+
 	res.status(200).json({
 		ok: true,
 		msg: "crearEvento",
