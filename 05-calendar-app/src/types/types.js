b4d22ea69cda9100f5befb93344a4b3480f20f7b
SoyDiego <diegofranchina@gmail.com>
2020-09-02 12:31:56 +0200

    Deployed backend in Heroku, created types and authReducer

--- 05-calendar-app/src/types/types.js
+++ 05-calendar-app/src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
 	eventClearActiveEvent: "[Events] Clear Active Event",
 	eventUpdated: "[Events] Event Updated",
 	eventDeleted: "[Events] Event Deleted",
+
+	authChecking: "[Auth] Checking login state",
+	authCheckingFinish: "[Auth] Checking finish state",
+	authStartLogin: "[Auth] Start login",
+	authLogin: "[Auth] Logged",
+	authStartRegister: "[Auth] Start Register",
+	authStartTokenRenew: "[Auth] Start Token Renew",
+	authLogout: "[Auth] Logout",
 };
