7572fad2ee650898b533a7259e65651fafa2fc96
SoyDiego <diegofranchina@gmail.com>
2020-09-05 10:24:18 +0200

    Build app moved to backend/public

---
+++ 05-calendar-app/backend/public/precache-manifest.6349d82ebae0eb05f672bcc1ed47afc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26493bbcb0181f61b0b6fa4da375243c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6221b27ddfe96f529ca",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "f58e78da48f89e1fae6a",
+    "url": "/static/css/main.68df9739.chunk.css"
+  },
+  {
+    "revision": "a6221b27ddfe96f529ca",
+    "url": "/static/js/2.5e39382d.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.5e39382d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58e78da48f89e1fae6a",
+    "url": "/static/js/main.34c60e05.chunk.js"
+  },
+  {
+    "revision": "f22525cd0eb73ebd53ee",
+    "url": "/static/js/runtime-main.70c83473.js"
+  }
+]);
